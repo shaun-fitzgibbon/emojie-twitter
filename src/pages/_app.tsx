1903563a2b36ed789e6d4038387e14e52f813f3c
Shaun Fitzgibbon <shaun@fitzindustries.com>
2023-03-27 13:22:37 +0200

    Profile page and ssg helper

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import { type AppType } from "next/app";
+import Head from "next/head";
 import { Toaster } from "react-hot-toast";
 
 import { api } from "~/utils/api";
@@ -9,6 +10,11 @@ import "~/styles/globals.css";
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
+      <Head>
+        <title>EmojiTwitter</title>
+        <meta name="description" content="💬" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Toaster position="bottom-center" />
       <Component {...pageProps} />
     </ClerkProvider>
