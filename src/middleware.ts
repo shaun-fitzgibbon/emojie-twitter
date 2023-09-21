a1aac205bdc2e248ea3b7f76f9d079429f7fd9f0
Shaun Fitzgibbon <shaun@fitzindustries.com>
2023-09-21 18:45:49 +0200

    update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,8 @@
-import { withClerkMiddleware } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
+import { authMiddleware } from "@clerk/nextjs/server";
 
-export default withClerkMiddleware(() => {
-  return NextResponse.next();
-});
+export default authMiddleware();
 
 // Stop Middleware running on static files
 export const config = {
-  matcher: "/((?!_next/image|_next/static|favicon.ico).*)",
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
