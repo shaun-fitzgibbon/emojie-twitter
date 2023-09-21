6a4cb6c536882a76eb9b52d3dee1a2d0b920116e
Shaun Fitzgibbon <shaun@fitzindustries.com>
2023-09-21 18:49:10 +0200

    change imageUrl

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -37,7 +37,7 @@ const CreatePostWizard = () => {
   return (
     <div className="flex w-full gap-3">
       <Image
-        src={user.profileImageUrl}
+        src={user.imageUrl}
         alt="Profile Image"
         className="h-14 w-14 rounded-full"
         width={56}
