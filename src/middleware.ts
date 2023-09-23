b992777cbbf5aee8f2be86626805ae9982533604
Shaun Fitzgibbon <shaun@fitzindustries.com>
2023-09-23 15:40:32 +0200

    public route keep-alive

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,8 @@
 import { authMiddleware } from "@clerk/nextjs/server";
 
-export default authMiddleware();
+export default authMiddleware({
+  publicRoutes: ["/api/cron/keep-alive"],
+});
 
 // Stop Middleware running on static files
 export const config = {
